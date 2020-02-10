jessebrennan/azul

commit c1476c79dc3aa60e69b2f76055defbb50a7c11a5

[1/2] Initialize boto session to use credential cache (#1534)
This ensures that you only have to reenter you MFA key when it
expires instead of once per session.

environment
@@ -416,7 +416,10 @@ _preauth () {
 
 			# Make the provider use the same cache as the AWS CLI
 			cli_cache = os.path.join(os.path.expanduser('~'), '.aws/cli/cache')
-			assume_role_provider.cache = botocore.credentials.JSONFileCache(cli_cache)
+			if not assume_role_provider.cache:
+                # We do this conditionally because envhook.py may have already
+                # set up the boto cache.
+			    assume_role_provider.cache = botocore.credentials.JSONFileCache(cli_cache)
 
 			# Request the credentials. If the CLI has cached credentials, this step
 			# would use those. If not, fresh ones will be requested from STS. If that

scripts/envhook.py
@@ -200,7 +200,7 @@ def sanitize_sys_path():
     whose name conflicts with that of important built-in or third-party
     packages, `json.py` for example. This project relies on the fully-qualified
     package path of those modules to disambiguate them from the built-in ones
-    but  placing their containing parent directory on `sys.path` defeats that.
+    but placing their containing parent directory on `sys.path` defeats that.
 
     This method attempts to counteract that by removing the directory again.
     """
@@ -210,8 +210,40 @@ def sanitize_sys_path():
     sys.meta_path.insert(0, SanitizingFinder())
 
 
+def use_cached_boto_session():
+    """
+    By default, boto does not use a cache for the assume-role provider. This
+    means that if assuming a role requires you to enter a MFA key, you will have
+    to enter this key every time you instantiate a boto session, even if your
+    assume-role session will last for longer.
+
+    This script connects the assume-role provider with the cache used by the
+    CLI, saving tedious key reentry.
+    """
+    try:
+        import boto3
+        import botocore.credentials
+        import botocore.session
+    except ImportError:
+        _print('Failed to import boto, so cached session will not be used.')
+    else:
+        # Get the AssumeRole credential provider and make it the only one
+        session = botocore.session.get_session()
+        resolver = session.get_component('credential_provider')
+        assume_role_provider = resolver.get_provider('assume-role')
+
+        # Make the provider use the same cache as the AWS CLI
+        cli_cache = os.path.join(os.path.expanduser('~'), '.aws/cli/cache')
+        assume_role_provider.cache = botocore.credentials.JSONFileCache(cli_cache)
+
+        # Every boto call will use this default session and therefore hit the
+        # cached credentials
+        boto3.setup_default_session(botocore_session=session)
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
 elif __name__ == 'sitecustomize':
     sanitize_sys_path()
     setenv()
+    use_cached_boto_session()

commit 6e37afebe08bc1340e3677584f5923aafa1b421d

Fix: _preauth does not persist credentials
Regression from c1476c79

environment
@@ -415,11 +415,8 @@ _preauth () {
 			resolver.providers = [assume_role_provider]
 
 			# Make the provider use the same cache as the AWS CLI
-			cli_cache = Path('~/.aws/cli/cache').expanduser()
-			if assume_role_provider.cache is None:
-			    # We do this conditionally because envhook.py may have already
-			    # set up the boto cache.
-			    assume_role_provider.cache = botocore.credentials.JSONFileCache(cli_cache)
+			cli_cache = Path('~', '.aws', 'cli', 'cache').expanduser()
+			assume_role_provider.cache = botocore.credentials.JSONFileCache(cli_cache)
 
 			# Request the credentials. If the CLI has cached credentials, this step
 			# would use those. If not, fresh ones will be requested from STS. If that

scripts/envhook.py
@@ -147,7 +147,7 @@ def zip_dict(old: Mapping[K, OV], new: Mapping[K, NV], missing=None) -> MutableM
 
 
 def _print(msg):
-    print(msg, file=sys.stderr)
+    print('envhook:', msg, file=sys.stderr)
 
 
 def _run(command) -> str:
@@ -238,7 +238,7 @@ def share_aws_cli_credential_cache():
         assume_role_provider = resolver.get_provider('assume-role')
 
         # Make the provider use the same cache as the AWS CLI
-        cli_cache = Path('~/.aws/cli/cache').expanduser()
+        cli_cache = Path('~', '.aws', 'cli', 'cache').expanduser()
         assume_role_provider.cache = botocore.credentials.JSONFileCache(cli_cache)
 
         # Calls to boto3.client() and .resource() use the default session and