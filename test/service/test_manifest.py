58d9ffa9eb8144f1fb08780a0fae12f6ac9fdf00
Noah Aviel Dove <nadove@ucsc.edu>
2021-03-18 21:07:33 -0700

    Add /repository/files URL to compact manifest (#2863)

--- test/service/test_manifest.py
+++ test/service/test_manifest.py
@@ -206,6 +206,12 @@ class TestManifestEndpoints(ManifestTestCase, DSSUnitTestCase):
              f'drs://{self.drs_domain}/5f9b45af-9a26-4b16-a785-7f2d1053dd7c?version=2018-09-14T123347.012715Z',
              f'drs://{self.drs_domain}/f2b6c6f0-8d25-4aae-b255-1974cc110cfe?version=2018-09-14T123343.720332Z'),
 
+            ('file_url',
+             f'{config.service_endpoint()}/repository/files/'
+             f'5f9b45af-9a26-4b16-a785-7f2d1053dd7c?version=2018-09-14T123347.012715Z&catalog=test',
+             f'{config.service_endpoint()}/repository/files/'
+             f'f2b6c6f0-8d25-4aae-b255-1974cc110cfe?version=2018-09-14T123343.720332Z&catalog=test'),
+
             ('cell_suspension.provenance.document_id',
              '',
              '0037c9eb-8038-432f-8d9d-13ee094e54ab || aaaaaaaa-8038-432f-8d9d-13ee094e54ab'),
